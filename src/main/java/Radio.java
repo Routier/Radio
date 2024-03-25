b27bfb3f476281a1ce8221adea6d922e462a73ad
num13 <num13-ne@mail.ru>
2024-03-25 17:56:41 +0300

    change construct

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,16 +1,16 @@
 public class Radio {
-    private int stationMin;
-    private int stationMax;
-    private int volumeMin;
-    private int volumeMax;
+    private int stationSize;
+    private int volumeMin = 0;
+    private int volumeMax = 100;
     private int currentStation;
     private int currentVolume;
 
-    public Radio(int stationMin, int stationMax, int volumeMin, int volumeMax) {
-        this.stationMin = stationMin;
-        this.stationMax = stationMax;
-        this.volumeMin = volumeMin;
-        this.volumeMax = volumeMax;
+    public Radio(int stationSize) {
+        this.stationSize = stationSize;
+    }
+
+    public Radio(){
+        this.stationSize = 10;
     }
 
     public int getCurrentStation() {
@@ -21,25 +21,13 @@ public class Radio {
         return currentVolume;
     }
 
-    public int getStationMin() {
-        return stationMin;
-    }
-
-    public int getStationMax() {
-        return stationMax;
-    }
-
-    public int getVolumeMin() {
-        return volumeMin;
-    }
-
-    public int getVolumeMax() {
-        return volumeMax;
+    public int getStationSize(){
+        return stationSize - 1;
     }
 
     public void setCurrentStation(int station) {
-        if (station > 9) {                      //выставление станции в пределах допустимого диапазона
-            currentStation = 9;
+        if (station > stationSize - 1) {             //выставление станции в пределах допустимого диапазона
+            currentStation = stationSize -1;
         } else if (station < 0) {
             currentStation = 0;
         } else {
@@ -58,18 +46,18 @@ public class Radio {
     }
 
     public void next() {
-        if (currentStation == 9) {                  //проверяет изначальное значение станции;
-            currentStation = 0;                     //если равно 9, то выставляет на 0
-        } else {                                    //во всех остальных случаях увеличивает значение на 1
+        if (currentStation == stationSize - 1) {
+            currentStation = 0;
+        } else {
             currentStation = currentStation + 1;
         }
     }
 
     public void prev() {
-        if (currentStation > 0) {                   //проверяет изначальное значение станции;
-            currentStation = currentStation - 1;    //уменьшает значение на 1, если значение юольше 0;
-        } else {                                    //если значение 0, то выставляет на 9
-            currentStation = 9;
+        if (currentStation > 0) {
+            currentStation = currentStation - 1;
+        } else {
+            currentStation = stationSize -1;
         }
     }
 
--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;;
 
 
 public class RadioTest {
-    Radio station = new Radio(0, 9, 0, 100);
+    Radio station = new Radio();
 
     @Test
     public void radioStationNextFromZero() {
@@ -22,7 +22,7 @@ public class RadioTest {
 
         station.prev();
 
-        int expected = 9;
+        int expected = station.getStationSize();
         int actual = station.getCurrentStation();
 
         Assertions.assertEquals(expected, actual);
@@ -31,7 +31,7 @@ public class RadioTest {
     @Test
     public void radioStationNextFromMax() {
 
-        station.setCurrentStation(9);
+        station.setCurrentStation(station.getStationSize() + 1);
         station.next();
 
         int expected = 0;
@@ -67,9 +67,9 @@ public class RadioTest {
     @Test
     public void radioStationSetOverMax() {
 
-        station.setCurrentStation(10);
+        station.setCurrentStation(station.getStationSize() + 1);
 
-        int expected = 9;
+        int expected = station.getStationSize();
         int actual = station.getCurrentStation();
 
         Assertions.assertEquals(expected, actual);
@@ -85,6 +85,95 @@ public class RadioTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    public void radioStationNextFromZeroVar() {
+        Radio station = new Radio(11);
+
+
+        station.next();
+        int expected = 1;
+        int actual = station.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void radioStationPrevFromZeroVar() {
+        Radio station = new Radio(11);
+
+        station.prev();
+
+        int expected = station.getStationSize();
+        int actual = station.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void radioStationNextFromMaxVar() {
+        Radio station = new Radio(11);
+
+        station.setCurrentStation(station.getStationSize() + 1);
+        station.next();
+
+        int expected = 0;
+        int actual = station.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void radioStationPrevFromMaxVar() {
+        Radio station = new Radio(11);
+
+
+        station.setCurrentStation(9);
+        station.prev();
+
+        int expected = 8;
+        int actual = station.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void radioStationSetVar() {
+        Radio station = new Radio(11);
+
+        station.setCurrentStation(5);
+
+        int expected = 5;
+        int actual = station.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void radioStationSetOverMaxVar() {
+        Radio station = new Radio(11);
+
+        station.setCurrentStation(station.getStationSize() + 1);
+
+        int expected = station.getStationSize();
+        int actual = station.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void radioStationSetLessThenZeroVar() {
+        Radio station = new Radio(11);
+
+        station.setCurrentStation(-1);
+
+        int expected = 0;
+        int actual = station.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 
     @Test
     public void radioVolumeIncreaseFromZero() {
